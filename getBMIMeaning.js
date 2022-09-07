c729893963f7ac1527fccd161736252ad32c413e
darKKOREO <tanapat.daoloy@mail.kmutt.ac.th>
2022-09-08 00:24:09 +0700

    1st commit

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,21 @@
 const { template } = require('@babel/core')
 
-function calculateBMI(weight, height) {
-  //code here
+function calculateBMI(weight,height) {
+  let calBMI = weight/(height^2)
+    return calBMI
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let BMI = calculateBMI(weight, height)
+  let result = ["Underweight","Normal weight","Overweight"]
+  if(BMI<18.5){
+    return result[0]
+  }
+  else if(18.5<=BMI && BMI<=24.9){
+    return result[1]
+  }else{
+    return result[2]
+  }
 }
+
+
 module.exports = getBMIMeaning
